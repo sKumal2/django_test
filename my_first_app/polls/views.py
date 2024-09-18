621ae79d7385d66d0a6cf9233765fe985b6cdb30
sKumal2 <kumalsamir08@gmail.com>
2024-09-18 10:54:05 -0400

    I am commiting on my new branch called old state and I added my first template here.

--- my_first_app/polls/views.py
+++ my_first_app/polls/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 #writing my first view
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     return HttpResponse("Hello World! You're at the polls index.")
+
+def members(request):
+    home = loader.get_template('home.html')
+    return HttpResponse(home.render())
