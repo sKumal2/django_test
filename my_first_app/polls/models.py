645e5e51f8d92837b9ef2b749af467c39dc7ffb2
sKumal2 <kumalsamir08@gmail.com>
2024-09-19 19:53:21 -0400

    I learnt to update and delete the records along with updating the model and also phone no requires BigIntegerField

--- my_first_app/polls/models.py
+++ my_first_app/polls/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Polls(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
+    phone = models.BigIntegerField(null=True)
+    email = models.EmailField(null=True)
