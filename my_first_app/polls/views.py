0bb52ccd23f42ffba9bcae460e524d3c7c373529
sKumal2 <kumalsamir08@gmail.com>
2024-09-24 16:21:39 -0400

    Fixed an error of adding links to html using views and templates

--- my_first_app/polls/views.py
+++ my_first_app/polls/views.py
@@ -7,7 +7,15 @@ from .models import Polls
 def home(request):
     home = loader.get_template('index.html')
     member = Polls.objects.all().values()
-    index = {
+    context = {
         'home' : member,
     }
-    return HttpResponse(home.render(index, request))
+    return HttpResponse(home.render(context, request))
+
+def details(request, id):
+    template = loader.get_template('details.html')
+    details = Polls.objects.get(id=id)
+    context = {
+        'details' : details
+    }
+    return HttpResponse(template.render(context, request))
