8211d93075904664e5b89ad9b8521ae110f48656
sKumal2 <kumalsamir08@gmail.com>
2024-09-26 11:22:58 -0400

    made Question and Choice views in Polls app and working on database

--- my_first_app/polls/models.py
+++ my_first_app/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Polls(models.Model):
@@ -6,3 +7,20 @@ class Polls(models.Model):
     lastname = models.CharField(max_length=20)
     phone = models.BigIntegerField(null=True)
     email = models.EmailField(null=True)
+
+class Question(models.Model):
+    que_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField("published date")
+    def __str__(self):
+        return self.que_text
+    def was_pub_rec(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    que = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choices = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choices
+    
+
